806023774e63ecdbe5aaf68a2ca599f4112fc5d8
kiril <kirill.301522@gmail.com>
2024-07-04 21:39:01 +1000

    back + sqllite

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './homepage.module.css';
-import {generateRandomItems} from '../../utils/generateRandomItems';
+import { fetchAdverts } from '../../api/advert';
+import {TItem} from '../../types/item.type';
 import LogoImage from '../../assets/images/adspotter_logo.png';
 import Panel from '../../ui/Panel/Panel';
 import Map from '../../ui/Map/Map';
 
 export default function HomePage() {
-  const list = generateRandomItems();
+    const [list, setList] = useState<TItem[]>([]);
 
-  return (
-    <>
-      <header className={styles.header}>
-        <div className={styles.wrapper}>
-          <a className={styles.link} href="/">
-            <img className={styles.image} src={LogoImage} alt="логотип" />
-          </a>
-        </div>
-      </header>
-      <main className={styles.main}>
-        <Panel list={list} />
-        <Map list={list} />
-      </main>
-    </>
-  );
+    useEffect(() => {
+        async function loadItems() {
+            try {
+                const items = await fetchAdverts();
+                console.log(items);
+                setList(items);
+            } catch (error) {
+                console.error('Error loading items:', error);
+            }
+        }
+        loadItems();
+    }, []);
+
+    return (
+        <>
+            <header className={styles.header}>
+                <div className={styles.wrapper}>
+                    <a className={styles.link} href="/">
+                        <img className={styles.image} src={LogoImage} alt="логотип" />
+                    </a>
+                </div>
+            </header>
+            <main className={styles.main}>
+                <Panel list={list} />
+                <Map list={list} />
+            </main>
+        </>
+    );
 }
