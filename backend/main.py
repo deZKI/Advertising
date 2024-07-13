444722b89f6f8a5b0e6a82d599228d0c78c131d8
kiril <kirill.301522@gmail.com>
2024-07-13 15:19:33 +1000

    fast api

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from routers import advert, optimization
 
-from ai.optimization import optimization as ai_optimization
+load_dotenv()
 
-app = FastAPI(docs_url='/api/swagger/')
+app = FastAPI(docs_url='/api/swagger')
 
-
-@app.post("api/advert")
-def advert():
-    return {"Hello": "World"}
-
-
-@app.post("api/optimization")
-def optimization(
-        age_from: int,
-        age_to: int,
-        name: str,
-        income: str,
-        gender: str,
-        iterations: int,
-        number_dots: int
-):
-    max_value, max_dots = ai_optimization(age_from, age_to, name, income, gender, number_dots,
-                                          iterations)
-    max_dots = [tuple(x) for x in max_dots[['lat', 'lon']].itertuples(index=False, name=None)]
-    return {'max_value': max_value, 'max_dots': max_dots}
+app.include_router(advert.router, prefix="/api/advert", tags=["Advert"])
+app.include_router(optimization.router, prefix="/api/optimization", tags=["Optimization"])
